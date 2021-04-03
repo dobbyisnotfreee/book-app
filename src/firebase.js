9ffad0a763ad734d01898c11b5e82761ff2c78b0
reactbacksu <80665940+reactbacksu@users.noreply.github.com>
2021-04-04 01:50:24 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDI4KIUot89vqqDK-PuLzrXPVqQAtNswDM",
-    authDomain: "book-app-test-70813.firebaseapp.com",
-    projectId: "book-app-test-70813",
-    storageBucket: "book-app-test-70813.appspot.com",
-    messagingSenderId: "538024152938",
-    appId: "1:538024152938:web:e47af86551f256553a1f13",
-    measurementId: "G-CBF3762XLV"
+
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
